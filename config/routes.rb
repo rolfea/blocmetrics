280181da77203c4a28da278d02e65c41425fa257
Alex Rolfe <rolfepercussion@gmail.com>
2016-03-10 23:27:41 -0600

    Adds events model and changes show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   resources :users
-  resources :registered_applications
+  resources :registered_applications do
+    resources :events, only: [:show]
+  end
+
   get 'welcome/index'
 
   get 'welcome/about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# creates users
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: "helloworld",
+    confirmed_at: "2016-02-15 18:00:00"
+  )
+end
+users = User.all
+
+# creates RegisteredApplications
+30.times do
+  RegisteredApplication.create!(
+    name: Faker::App.name,
+    URL: Faker::Internet.url,
+    user_id: rand(1..10)
+  )
+end
+registered_applications = RegisteredApplication.all
+
+60.times do
+  Event.create!(
+    name: Faker::Hipster.word,
+    registered_application_id: rand(1..30)
+  )
+end
+
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{RegisteredApplication.count} wikis created"
+puts "#{Event.count} events created"
